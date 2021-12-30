073b610352f5f9b8257cbad6e28896b918328f0e
Tyler Rhodes <rhodetyl000@gmail.com>
2021-12-30 17:09:06 -0600

    Add notes for internal field separator

--- bashvariables.sh
+++ bashvariables.sh
@@ -1,10 +1,10 @@
 #! /usr/local/bin/bash
 
-
 echo "double digit arguments must be specified this way: ${12}"
 echo " this is the number of parameters passed to the program: $#"
 echo " this is the current exit code: $?"
 echo "this is the exact path of the script: $0"
+echo "this is the internal field seperator: ---> $IFS <---"
 
 echo "all parameters: $@"
 echo "all parameters: $*"
@@ -41,3 +41,35 @@ for i in "$*"; do
 done
 ########################
 
+
+### IFS ################
+x="aa bb cc dd";
+
+for i in $x
+do
+  echo $i
+done
+
+echo -e "\n"
+
+OLD_IFS=$IFS
+IFS="_"
+
+x="aa_bb_cc_dd";
+
+for i in $x
+do
+  echo $i
+done
+
+
+read a b c d e <<< "hi_there_how_are_you"
+echo "a is $a"
+echo "b is $b"
+echo "c is $c"
+echo "d is $d"
+echo "e is $e"
+
+IFS=$OLD_IFS
+
+########################
